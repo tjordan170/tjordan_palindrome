1cd31e75159eff4369a6e13cd6cfb6f50ba1baf4
Thomas Jordan <thomas.jordan@gmail.com>
2022-01-11 20:01:45 -0700

    Finish working and refactored palindrome method

--- lib/tjordan_palindrome.rb
+++ lib/tjordan_palindrome.rb
@@ -2,7 +2,22 @@
 
 require_relative "tjordan_palindrome/version"
 
-module TjordanPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+class String
+
+  # Returns true for a palindrome, false otherwise.
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  # Returns the letters in the string.
+  def letters
+    self.chars.select { |c| c.match(/[a-z]/i) }.join
+  end
+
+  private
+
+    # Returns content for palindrome testing.
+    def processed_content
+      scan(/[a-z]/i).join.downcase
+    end
 end
--- test/test_tjordan_palindrome.rb
+++ test/test_tjordan_palindrome.rb
@@ -3,11 +3,20 @@
 require "test_helper"
 
 class TestTjordanPalindrome < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::TjordanPalindrome::VERSION
+  def test_non_palindrome
+    assert !"apple".palindrome?
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_literal_palindrome
+    assert "racecar".palindrome?
   end
+
+  def test_mixed_case_palindrome
+    assert "RaCecar".downcase.palindrome?
+  end
+
+  def test_palindrome_with_punctuation
+    assert "Madam, I'm Adam.".palindrome?
+  end
+
 end
